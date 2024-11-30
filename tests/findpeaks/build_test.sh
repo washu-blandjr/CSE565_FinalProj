5b29c95a1a342123d0365834f93ddd5607a18370
huss2342 <a.hussein@wustl.edu>
2024-11-30 09:59:19 -0600

    updated test

--- tests/findpeaks/build_test.sh
+++ tests/findpeaks/build_test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 g++ -o test_findpeaks test_findpeaks.cpp ../../findpeaks.cpp -std=c++17 -Wall -Wextra -O2
-./test_findpeaks
+./test_findpeaks > output.txt
+if diff output.txt golden.txt > /dev/null; then
+    echo "!Output matches golden file!"
+else
+    echo "XXX Output differs from golden file XXX"
+    diff output.txt golden
+fi
+
